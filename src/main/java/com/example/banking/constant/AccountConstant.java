ba2ab73f34627e703207fee365e2fa22d5f86031
Rinkesh <rinkeshraj091@gmail.com>
2024-10-05 16:38:21 +0530

    Add CustomHandler

--- src/main/java/com/example/banking/constant/AccountConstant.java
+++ src/main/java/com/example/banking/constant/AccountConstant.java
@@ -6,4 +6,5 @@ public class AccountConstant {
     public static final String CREATE ="/create";
     public static final String GET_ACCOUNT ="/accounts/{phoneNumber}";
     public static final String ERROR_MSG_1 ="Phone Number is not present";
+    public static final String NOT_EXISTS ="Record already exists";
 }
---
+++ src/main/java/com/example/banking/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.banking.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Map<String, String>> handleCustomException(CustomException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", String.valueOf(ex.getStatus()));
+        response.put("code", ex.getCode());
+        response.put("title", ex.getTitle());
+        response.put("detail", ex.getDetail());
+        HttpStatus status = ex.getStatus();
+
+        return new ResponseEntity<>(response, status); // Change status as needed
+    }
+}
